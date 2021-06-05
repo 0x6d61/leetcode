6cf2e7c8f2276f62310204df0bed9d30e1200878
0x6d61 <0x6d61@gmail.com>
2021-06-06 05:00:50 +0900

    add P709

---
+++ P709/P709.java
@@ -0,0 +1,14 @@
+package P709;
+
+public class P709 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.toLowerCase("Hello"));
+    }
+}
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
